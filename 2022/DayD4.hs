aad900f0f764412d1d76a0f68340b6a08c0b8e0c
Marienkos <96983609+Marienkos@users.noreply.github.com>
2022-12-11 12:12:45 +0100

    Update DayD4.hs

--- 2022/DayD4.hs
+++ 2022/DayD4.hs
@@ -1,19 +1,19 @@
-sep :: [Char] -> (String, String)
-sep x = (takeWhile (/= ',') x, tail $ dropWhile (/= ',') x)
+split :: [Char] -> (String, String)
+split x = (takeWhile (/= ',') x, tail $ dropWhile (/= ',') x)
 
-allz :: [Int] -> [Int] -> Int
-allz x y
+intersect :: [Int] -> [Int] -> Int
+intersect x y
     | length [e | e <- x, elem e x, elem e y] > 0 = 1
     | otherwise = 0
 
-val :: (String, String) -> Int
-val (x, y) = allz (int x) (int y)
+evaluate :: (String, String) -> Int
+evaluate (x, y) = intersect (int x) (int y)
     where int x = [(read (takeWhile (/= '-') x) :: Int)..(read (tail $ dropWhile (/= '-') x) :: Int)]
 
-sol :: [Char] -> Int
-sol l = sum [val $ sep x | x <- lines l]
+solve :: [Char] -> Int
+solve l = sum [evaluate $ split x | x <- lines l]
 
 main :: IO ()
 main = do
     input <- readFile "input.txt"
-    print $ sol input
+    print $ solve input
