210dde95a27f1690fd8f6aeefdf42bddf4329c51
Marienkos <96983609+Marienkos@users.noreply.github.com>
2022-12-11 12:13:02 +0100

    Update DayE5.hs

--- 2022/DayE5.hs
+++ 2022/DayE5.hs
@@ -1,8 +1,8 @@
-cmn :: [String] -> [String]
-cmn m = [[m !! x !! y | x <- [0..7], m !! x !! y /= ' '] | y <- [1, 5, 9, 13, 17, 21, 25, 29, 33]]
+column :: [String] -> [String]
+column m = [[m !! x !! y | x <- [0..7], m !! x !! y /= ' '] | y <- [1, 5, 9, 13, 17, 21, 25, 29, 33]]
 
-mov :: Int -> Int -> Int -> [[Char]] -> [[Char]]
-mov n s z m
+move :: Int -> Int -> Int -> [[Char]] -> [[Char]]
+move n s z m
     | s <= z = prima ++ da ++ mezzo ++ verso ++ dopo
     | z < s = prima ++ verso ++ mezzo ++ da ++ dopo
         where
@@ -12,23 +12,23 @@ mov n s z m
             mezzo = drop (min s z + 1) (take (max s z) m)
             dopo = drop (max s z + 1) m
 
-num :: [Char] -> [Int]
-num l
+numerate :: [Char] -> [Int]
+numerate l
     | length numeri == 3 = [numeri !! 0, (numeri !! 1)-1, (numeri !! 2)-1]
     | otherwise = [(numeri !! 0)*10 + (numeri !! 1), (numeri !! 2)-1, (numeri !! 3)-1] 
         where numeri = [read [x] :: Int | x <- l, elem x ['0'..'9']]
 
 nll :: [String] -> [[Int]]
-nll m = [num x | x <- [m !! y | y <- [10..(length m)-1]]]
+nll m = [numerate x | x <- [m !! y | y <- [10..(length m)-1]]]
 
-exe :: [[Char]] -> Int -> [[Char]]
-exe x (-1) = cmn x
-exe x y = mov ((nll x) !! y !! 0) ((nll x) !! y !! 1) ((nll x) !! y !! 2) (exe x (y-1))
+execute :: [[Char]] -> Int -> [[Char]]
+execute x (-1) = column x
+execute x y = move ((nll x) !! y !! 0) ((nll x) !! y !! 1) ((nll x) !! y !! 2) (execute x (y-1))
 
-sol :: [Char] -> [Char]
-sol x = map head (exe (lines x) ((length $ nll $ lines x)-1))
+solve :: [Char] -> [Char]
+solve x = map head (execute (lines x) ((length $ nll $ lines x)-1))
 
 main :: IO ()
 main = do
     input <- readFile "input.txt"
-    print $ sol input
+    print $ solve input
