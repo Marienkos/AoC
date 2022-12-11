841431d02c9fd5f8f9a49cb71982cc05babd880f
Marienkos <96983609+Marienkos@users.noreply.github.com>
2022-12-11 12:13:16 +0100

    Update DayF6.hs

--- 2022/DayF6.hs
+++ 2022/DayF6.hs
@@ -1,15 +1,15 @@
-dif :: [Char] -> Bool
-dif l = length l == length [x | x <- ['a'..'z'], elem x l]
+different :: [Char] -> Bool
+different l = length l == length [x | x <- ['a'..'z'], elem x l]
 
-val :: [Char] -> [Int]
-val (x:xs)
-    | dif $ take 14 (x:xs) = [14]
-    | otherwise = 1 : (val xs)
+evaluate :: [Char] -> [Int]
+evaluate (x:xs)
+    | different $ take 14 (x:xs) = [14]
+    | otherwise = 1 : (evaluate xs)
 
-sol :: [Char] -> Int
-sol = sum . val
+solve :: [Char] -> Int
+solve = sum . val
 
 main :: IO ()
 main = do
     input <- readFile "input.txt"
-    print $ sol input
+    print $ solve input
