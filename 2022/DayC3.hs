b1f196c58e31f9f20d41553599ea84e40bad63f4
Marienkos <96983609+Marienkos@users.noreply.github.com>
2022-12-11 12:12:31 +0100

    Update DayC3.hs

--- 2022/DayC3.hs
+++ 2022/DayC3.hs
@@ -1,19 +1,19 @@
-tpz :: [String] -> [[String]]
-tpz [] = []
-tpz x = (take 3 x) : (tpz $ drop 3 x)
+tripartize :: [String] -> [[String]]
+tripartize [] = []
+tripartize x = (take 3 x) : (tripartize $ drop 3 x)
 
 cml :: [String] -> [Char]
-cml l = [com x | x <- tpz l]
+cml l = [com x | x <- tripartize l]
     where com [x, y, z] = head [c | c <- ['A'..'z'], elem c x, elem c y, elem c z]
 
 conv :: [Char] -> [Int]
 conv l = [letter x | x <- l]
     where letter c = head [x | x <- [1..52], (['a'..'z']++['A'..'Z']) !! (x-1) == c]
 
-sol :: [Char] -> Int
-sol = sum . conv . cml . lines
+solve :: [Char] -> Int
+solve = sum . conv . cml . lines
 
 main :: IO ()
 main = do
     input <- readFile "input.txt"
-    print $ sol input
+    print $ solve input
