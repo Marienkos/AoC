a39145599d485baadee4205e280f6e99bba5491e
Marienkos <96983609+Marienkos@users.noreply.github.com>
2022-12-11 12:14:22 +0100

    Update DayJ10.hs

--- 2022/DayJ10.hs
+++ 2022/DayJ10.hs
@@ -1,23 +1,23 @@
-prs :: [String] -> [Int]
-prs [] = []
-prs (x:xs)
-    | (words x) !! 0 == "noop" = 0 : (prs xs)
-    | otherwise = [0, read ((words x) !! 1) :: Int] ++ (prs xs)
+parse :: [String] -> [Int]
+parse [] = []
+parse (x:xs)
+    | (words x) !! 0 == "noop" = 0 : (parse xs)
+    | otherwise = [0, read ((words x) !! 1) :: Int] ++ (parse xs)
 
-val :: [Char] -> [Int]
-val x = [1 + sum [(prs $ lines x) !! n | n <- [0..c-2]] | c <- [1..240]]
+evaluate :: [Char] -> [Int]
+evaluate x = [1 + sum [(parse $ lines x) !! n | n <- [0..c-2]] | c <- [1..240]]
 
-drw :: Int -> [Int] -> [Char]
-drw _ [] = []
-drw n (x:xs)
-    | n == 40 = '\n' : (drw 0 (x:xs))
-    | elem n [x-1, x, x+1] = '#' : (drw (n+1) xs)
-    | otherwise = '.' : (drw (n+1) xs)
+draw :: Int -> [Int] -> [Char]
+draw _ [] = []
+draw n (x:xs)
+    | n == 40 = '\n' : (draw 0 (x:xs))
+    | elem n [x-1, x, x+1] = '#' : (draw (n+1) xs)
+    | otherwise = '.' : (draw (n+1) xs)
 
-sol :: [Char] -> [Char]
-sol x = drw 0 (val x)
+solve :: [Char] -> [Char]
+solve x = draw 0 (evaluate x)
 
 main :: IO ()
 main = do
     input <- readFile "input.txt"
-    putStrLn $ sol input
+    putStrLn $ solve input
