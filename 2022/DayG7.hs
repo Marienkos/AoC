bbbfe47f62eec5051df70cb49719985e42dae4b9
Marienkos <96983609+Marienkos@users.noreply.github.com>
2022-12-11 12:13:41 +0100

    Update DayG7.hs

--- 2022/DayG7.hs
+++ 2022/DayG7.hs
@@ -2,63 +2,63 @@ coda :: [a] -> [a]
 coda [] = []
 coda (x:xs) = xs
 
-spt :: String -> [String]
-spt [] = []
-spt x = [(takeWhile (/= '/') x)] ++ (spt $ coda (dropWhile (/= '/') x))
+split :: String -> [String]
+split [] = []
+split x = [(takeWhile (/= '/') x)] ++ (split $ coda (dropWhile (/= '/') x))
 
-cat :: [String] -> String
-cat [] = []
-cat (x:xs)
-    | length (x:xs) > 1 = x ++ "/" ++ (cat xs)
+unify :: [String] -> String
+unify [] = []
+unify (x:xs)
+    | length (x:xs) > 1 = x ++ "/" ++ (unify xs)
     | otherwise = []
 
-prs :: String -> [String] -> [String]
-prs s [] = [s]
-prs s (x:xs)
-    | x == "$ cd /" = prs "/" xs
-    | and [length parole == 3, parole !! 2 /= ".."] = s : (prs (s ++ (parole !! 2) ++ "/") xs)
-    | and [length parole == 3, parole !! 2 == ".."] = s : (prs (cat $ spt s) xs)
-    | or [parole !! 1 == "ls", parole !! 0 == "dir"] = prs s xs
-    | otherwise = (s ++ (parole !! 0)) : (prs s xs)
+parse :: String -> [String] -> [String]
+parse s [] = [s]
+parse s (x:xs)
+    | x == "$ cd /" = parse "/" xs
+    | and [length parole == 3, parole !! 2 /= ".."] = s : (parse (s ++ (parole !! 2) ++ "/") xs)
+    | and [length parole == 3, parole !! 2 == ".."] = s : (parse (unify $ split s) xs)
+    | or [parole !! 1 == "ls", parole !! 0 == "dir"] = parse s xs
+    | otherwise = (s ++ (parole !! 0)) : (parse s xs)
         where parole = words x
 
-fil :: [String] -> [String]
-fil [] = []
-fil (x:xs)
-    | last x /= '/' = x : (fil xs)
-    | otherwise = fil xs
+file :: [String] -> [String]
+file [] = []
+file (x:xs)
+    | last x /= '/' = x : (file xs)
+    | otherwise = file xs
 
-fld :: [String] -> [String]
-fld [] = []
-fld (x:xs)
-    | last x == '/' = x : (fld xs)
-    | otherwise = fld xs
+folder :: [String] -> [String]
+folder [] = []
+folder (x:xs)
+    | last x == '/' = x : (folder xs)
+    | otherwise = folder xs
 
-dup :: [String] -> [String]
-dup [] = []
-dup (x:xs)
-    | elem x xs = dup xs
-    | otherwise = x : (dup xs)
+noduplicates :: [String] -> [String]
+noduplicates [] = []
+noduplicates (x:xs)
+    | elem x xs = noduplicates xs
+    | otherwise = x : (noduplicates xs)
 
-infi :: String -> String -> Bool
-infi _ [] = False
-infi x (y:ys)
+substring :: String -> String -> Bool
+substring _ [] = False
+substring x (y:ys)
     | take (length x) (y:ys) == x = True
-    | otherwise = infi x ys
+    | otherwise = substring x ys
 
-som :: String -> [String] -> Int
-som _ [] = 0
-som c (f:fs)
-    | infi c f = (read (last $ spt f) :: Int) + (som c fs)
-    | otherwise = som c fs
+sums :: String -> [String] -> Int
+sums _ [] = 0
+sums c (f:fs)
+    | substring c f = (read (last $ split f) :: Int) + (sums c fs)
+    | otherwise = sums c fs
 
-sol :: [Char] -> Int
-sol i = minimum [som x files | x <- folders, 70000000-(som "/" files)+(som x files)>=30000000]
+solve :: [Char] -> Int
+solve i = minimum [sums x files | x <- folders, 70000000-(sums "/" files)+(sums x files)>=30000000]
     where
-        files = fil $ prs " " (lines i)
-        folders = dup $ fld $ prs " " (lines i)
+        files = file $ parse " " (lines i)
+        folders = noduplicates $ folder $ parse " " (lines i)
 
 main :: IO ()
 main = do
     input <- readFile "input.txt"
-    print $ sol input
+    print $ solve input
