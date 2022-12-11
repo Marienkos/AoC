5b1585aaee35398211f92485c69716010d65d855
Marienkos <96983609+Marienkos@users.noreply.github.com>
2022-12-11 12:13:55 +0100

    Update DayH8.hs

--- 2022/DayH8.hs
+++ 2022/DayH8.hs
@@ -1,28 +1,28 @@
-prs :: [String] -> [[Int]]
-prs m = [[read [x] :: Int | x <- y] | y <- m]
+parse :: [String] -> [[Int]]
+parse m = [[read [x] :: Int | x <- y] | y <- m]
 
 
-lwr :: Int -> [Int] -> Int
-lwr _ [] = 0
-lwr n (x:xs)
-    | x < n = 1 + (lwr n xs)
+lower :: Int -> [Int] -> Int
+lower _ [] = 0
+lower n (x:xs)
+    | x < n = 1 + (lower n xs)
     | otherwise = 1
 
-vis :: Int -> Int -> [[Int]] -> Int
-vis a o m = product $ map (lwr (m !! a !! o)) [n, s, w, e]
+visible :: Int -> Int -> [[Int]] -> Int
+visible a o m = product $ map (lower (m !! a !! o)) [n, s, w, e]
     where
         n = reverse [m !! x !! o | x <- [0..a-1]]
         s = [m !! x !! o | x <- [a+1..98]]
         w = reverse [m !! a !! y | y <- [0..o-1]]
         e = [m !! a !! y | y <- [o+1..98]]
 
-alv :: [[Int]] -> Int
-alv m = maximum [vis x y m | x <- [0..98], y <- [0..98]]
+allvisible :: [[Int]] -> Int
+allvisible m = maximum [visible x y m | x <- [0..98], y <- [0..98]]
 
-sol :: [Char] -> Int
-sol = alv . prs . lines
+solve :: [Char] -> Int
+solve = allvisible . parse . lines
 
 main :: IO ()
 main = do
     input <- readFile "input.txt"
-    print $ sol input
+    print $ solve input
