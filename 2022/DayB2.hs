49292fcd3df4682d5818dbcd02b1a469ad7c877b
Marienkos <96983609+Marienkos@users.noreply.github.com>
2022-12-11 12:12:09 +0100

    Update DayB2.hs

--- 2022/DayB2.hs
+++ 2022/DayB2.hs
@@ -1,18 +1,18 @@
-pts :: [Char] -> Int
-pts "A X" = 3
-pts "A Y" = 4
-pts "A Z" = 8
-pts "B X" = 1
-pts "B Y" = 5
-pts "B Z" = 9
-pts "C X" = 2
-pts "C Y" = 6
-pts "C Z" = 7
+points :: [Char] -> Int
+points "A X" = 3
+points "A Y" = 4
+points "A Z" = 8
+points "B X" = 1
+points "B Y" = 5
+points "B Z" = 9
+points "C X" = 2
+points "C Y" = 6
+points "C Z" = 7
 
-sol :: [Char] -> Int
-sol l = sum [pts x | x <- lines l]
+solve :: [Char] -> Int
+solve l = sum [points x | x <- lines l]
 
 main :: IO ()
 main = do
     input <- readFile "input.txt"
-    print $ sol input
+    print $ solve input
