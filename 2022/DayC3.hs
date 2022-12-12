fa69233670ed25bb9227b966e37a47aca4013fa5
Marienkos <96983609+Marienkos@users.noreply.github.com>
2022-12-12 09:20:18 +0100

    Update DayC3.hs

--- 2022/DayC3.hs
+++ 2022/DayC3.hs
@@ -2,12 +2,12 @@ tripartize :: [String] -> [[String]]
 tripartize [] = []
 tripartize x = (take 3 x) : (tripartize $ drop 3 x)
 
-cml :: [String] -> [Char]
-cml l = [com x | x <- tripartize l]
-    where com [x, y, z] = head [c | c <- ['A'..'z'], elem c x, elem c y, elem c z]
+threepartcommon :: [String] -> [Char]
+threepartcommon l = [commmon x | x <- tripartize l]
+    where commmon [x, y, z] = head [c | c <- ['A'..'z'], elem c x, elem c y, elem c z]
 
-conv :: [Char] -> [Int]
-conv l = [letter x | x <- l]
+convert :: [Char] -> [Int]
+convert l = [letter x | x <- l]
     where letter c = head [x | x <- [1..52], (['a'..'z']++['A'..'Z']) !! (x-1) == c]
 
 solve :: [Char] -> Int
