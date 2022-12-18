651cb0b6096e6485f6cc16f0f157b2602a7a5829
Marienkos <96983609+Marienkos@users.noreply.github.com>
2022-12-18 11:46:27 +0100

    Add files via upload

---
+++ 2022/Day18.hs
@@ -0,0 +1,21 @@
+split :: String -> [String]
+split [] = []
+split x = [takeWhile (/= ',') x] ++ (split $ drop 1 (dropWhile (/= ',') x))
+
+parse :: String -> (Int, Int, Int)
+parse x = (read (split x !! 0) :: Int, read (split x !! 1) :: Int, read (split x !! 2) :: Int)
+
+sides :: (Int, Int, Int) -> [(Int, Int, Int)] -> Int
+sides (x, y, z) lava = 6 - (sum $ map (fromEnum . inlist) [(x, y-1, z), (x, y+1, z), (x-1, y, z), (x+1, y, z), (x, y, z-1), (x, y, z+1)])
+    where inlist element = elem element lava
+
+solve :: [Char] -> Int
+solve x = sum $ map cube list
+    where
+        list = map parse (lines x)
+        cube x = sides x list
+
+main :: IO ()
+main = do
+    input <- readFile "input.txt"
+    print $ solve input
