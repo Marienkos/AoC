00b5a37ec282040438ad4f0caeadaea0570ac91d
Marienkos <96983609+Marienkos@users.noreply.github.com>
2022-12-25 10:11:50 +0100

    Update and rename DayG7.hs to Day07.hs

--- 2022/Day07.hs
+++ 2022/Day07.hs
@@ -1,40 +1,40 @@
 split :: String -> [String]
 split [] = []
-split x = [(takeWhile (/= '/') x)] ++ (split $ drop 1 (dropWhile (/= '/') x))
+split x = takeWhile (/= '/') x : split (drop 1 (dropWhile (/= '/') x))
 
 unify :: [String] -> String
 unify [] = []
 unify (x:xs)
-    | length (x:xs) > 1 = x ++ "/" ++ (unify xs)
+    | length (x:xs) > 1 = x ++ "/" ++ unify xs
     | otherwise = []
 
 parse :: String -> [String] -> [String]
 parse s [] = [s]
 parse s (x:xs)
     | x == "$ cd /" = parse "/" xs
-    | and [length parole == 3, parole !! 2 /= ".."] = s : (parse (s ++ (parole !! 2) ++ "/") xs)
-    | and [length parole == 3, parole !! 2 == ".."] = s : (parse (unify $ split s) xs)
-    | or [parole !! 1 == "ls", parole !! 0 == "dir"] = parse s xs
-    | otherwise = (s ++ (parole !! 0)) : (parse s xs)
+    | (length parole == 3) && (parole !! 2 /= "..") = s : parse (s ++ (parole !! 2) ++ "/") xs
+    | (length parole == 3) && (parole !! 2 == "..") = s : parse (unify $ split s) xs
+    | (parole !! 1 == "ls") || (head parole == "dir") = parse s xs
+    | otherwise = (s ++ head parole) : parse s xs
         where parole = words x
 
 file :: [String] -> [String]
 file [] = []
 file (x:xs)
-    | last x /= '/' = x : (file xs)
+    | last x /= '/' = x : file xs
     | otherwise = file xs
 
 folder :: [String] -> [String]
 folder [] = []
 folder (x:xs)
-    | last x == '/' = x : (folder xs)
+    | last x == '/' = x : folder xs
     | otherwise = folder xs
 
 noduplicates :: [String] -> [String]
 noduplicates [] = []
 noduplicates (x:xs)
     | elem x xs = noduplicates xs
-    | otherwise = x : (noduplicates xs)
+    | otherwise = x : noduplicates xs
 
 substring :: String -> String -> Bool
 substring _ [] = False
@@ -45,11 +45,11 @@ substring x (y:ys)
 sums :: String -> [String] -> Int
 sums _ [] = 0
 sums c (f:fs)
-    | substring c f = (read (last $ split f) :: Int) + (sums c fs)
+    | substring c f = (read (last $ split f) :: Int) + sums c fs
     | otherwise = sums c fs
 
 solve :: [Char] -> Int
-solve i = minimum [sums x files | x <- folders, 70000000-(sums "/" files)+(sums x files)>=30000000]
+solve i = minimum [sums x files | x <- folders, 70000000-sums "/" files+sums x files>=30000000]
     where
         files = file $ parse " " (lines i)
         folders = noduplicates $ folder $ parse " " (lines i)
