ddaa5a5099c0272b559f6f48668fb20a705dd99a
Marienkos <96983609+Marienkos@users.noreply.github.com>
2022-12-25 10:11:07 +0100

    Update DayF6.hs

--- 2022/DayF6.hs
+++ 2022/DayF6.hs
@@ -4,7 +4,7 @@ different l = length l == length [x | x <- ['a'..'z'], elem x l]
 evaluate :: [Char] -> [Int]
 evaluate (x:xs)
     | different $ take 14 (x:xs) = [14]
-    | otherwise = 1 : (evaluate xs)
+    | otherwise = 1 : evaluate xs
 
 solve :: [Char] -> Int
 solve = sum . val
