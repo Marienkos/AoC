d35542b9504aa3286c4b6e27af2a0ef5e6db9f2a
Marienkos <96983609+Marienkos@users.noreply.github.com>
2022-12-25 10:09:11 +0100

    Update and rename DayA1.hs to Day01.hs

--- 2022/Day01.hs
+++ 2022/Day01.hs
@@ -1,12 +1,12 @@
 numerate :: [String] -> [Int]
 numerate [] = []
-numerate ("":xs) = 0 : (numerate xs)
-numerate (x:xs) = (read x :: Int) : (numerate xs)
+numerate ("":xs) = 0 : numerate xs
+numerate (x:xs) = (read x :: Int) : numerate xs
 
 somme :: [Int] -> [Int]
 somme [] = []
 somme (0:xs) = somme xs
-somme x = (sum $ takeWhile (/= 0) x) : (somme $ dropWhile (/= 0) x)
+somme x = sum (takeWhile (/= 0) x) : somme (dropWhile (/= 0) x)
 
 senza :: [Int] -> Int -> [Int]
 senza l m = [x | x <- l, x /= m]
@@ -14,7 +14,7 @@ senza l m = [x | x <- l, x /= m]
 top :: [Int] -> Int
 top x = sum [maximum x, maximum $ senza x (maximum x), maximum $ senza (senza x (maximum x)) (maximum $ senza x (maximum x))]
 
-solve :: [Char] -> Int
+solve :: String -> Int
 solve = top . somme . numerate . lines
 
 main :: IO ()
