2282844e93e5714c19348b4a1016052b31d94e4e
Marienkos <96983609+Marienkos@users.noreply.github.com>
2022-12-27 15:47:53 +0100

    Update Day18.hs

--- 2022/Day18.hs
+++ 2022/Day18.hs
@@ -6,8 +6,7 @@ parse :: String -> (Int, Int, Int)
 parse x = (read (head (split x)) :: Int, read (split x !! 1) :: Int, read (split x !! 2) :: Int)
 
 sides :: (Int, Int, Int) -> [(Int, Int, Int)] -> Int
-sides (x, y, z) lava = 6 - sum (map (fromEnum . inlist) [(x, y-1, z), (x, y+1, z), (x-1, y, z), (x+1, y, z), (x, y, z-1), (x, y, z+1)])
-    where inlist element = elem element lava
+sides (x, y, z) lava = 6 - sum (map (fromEnum . (`elem` lava)) [(x, y-1, z), (x, y+1, z), (x-1, y, z), (x+1, y, z), (x, y, z-1), (x, y, z+1)])
 
 solve :: [Char] -> Int
 solve x = sum $ map cube list
