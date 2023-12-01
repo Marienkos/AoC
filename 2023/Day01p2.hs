12862ff7bdd8157dd188abff5bb9f490f5536b1f
Marienkos <96983609+Marienkos@users.noreply.github.com>
2023-12-01 23:21:31 +0100

    Update Day01p2.hs

--- 2023/Day01p2.hs
+++ 2023/Day01p2.hs
@@ -1,23 +1,13 @@
-isin :: Char -> [Char] -> Bool
-isin _ [] = False
-isin c (x:xs)
-    | c == x = True
-    | otherwise = isin c xs
-
-substring :: [String] -> String -> Int -> Int
-substring _ [] _ = 0
-substring [] _ _ = 0
-substring (x:xs) y n
-    | take (length x) y == x = n
-    | otherwise = substring xs y (n+1)
+convert :: [(Char, String)] -> String -> [Int]
+convert _ [] = []
+convert [] _ = []
+convert (x:xs) y
+    | fst x == head y || snd x == take (length (snd x)) y = [read [fst x] :: Int]
+    | otherwise = substring xs y
 
 decode :: [Char] -> [Int]
 decode [] = []
-decode (x:xs)
-    | isin x ['1'..'9'] = (read [x] :: Int) : decode xs
-    | isNumber /= 0 = isNumber : decode xs
-    | otherwise = decode xs
-        where isNumber = substring ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"] (x:xs) 1
+decode (x:xs) = convert [('1', "one"), ('2', "two"), ('3', "three"), ('4', "four"), ('5', "five"), ('6', "six"), ('7', "seven"), ('8', "eight"), ('9', "nine")] (x:xs) ++ decode xs
 
 value :: [Int] -> Int
 value list = (head list) * 10 + last list
