8aa4864aa9aceeb8cbee13b5dbdd30cce07d69d6
Marienkos <96983609+Marienkos@users.noreply.github.com>
2023-12-01 23:10:06 +0100

    Update Day01p2.hs

--- 2023/Day01p2.hs
+++ 2023/Day01p2.hs
@@ -4,20 +4,20 @@ isin c (x:xs)
     | c == x = True
     | otherwise = isin c xs
 
-substring :: [String] -> String -> Int -> (Int, Bool)
-substring _ [] _ = (0, False)
-substring [] _ _ = (0, False)
+substring :: [String] -> String -> Int -> Int
+substring _ [] _ = 0
+substring [] _ _ = 0
 substring (x:xs) y n
-    | take (length x) y == x = (n, True)
+    | take (length x) y == x = n
     | otherwise = substring xs y (n+1)
 
 decode :: [Char] -> [Int]
 decode [] = []
-decode list@(x:xs)
+decode (x:xs)
     | isin x ['1'..'9'] = (read [x] :: Int) : decode xs
-    | snd isNumber = (fst isNumber) : decode xs
+    | isNumber /= 0 = isNumber : decode xs
     | otherwise = decode xs
-        where isNumber = substring ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"] list 1
+        where isNumber = substring ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"] (x:xs) 1
 
 value :: [Int] -> Int
 value list = (head list) * 10 + last list
