bf0c696ab7c498e72df2ad38d79f5f4ef8948d2f
Marienkos <96983609+Marienkos@users.noreply.github.com>
2023-12-02 17:32:59 +0100

    Update Day02p2.hs

--- 2023/Day02p2.hs
+++ 2023/Day02p2.hs
@@ -8,18 +8,15 @@ only x = case drop 1 (dropWhile (/= ':') x) of
     "" -> x
     _ -> drop 1 (dropWhile (/= ':') x)
 
-retrieve :: (Int, Int, Int) -> [[String]] -> (Int, Int, Int)
-retrieve colors [] = colors
-retrieve (r, g, b) (x:xs)
-    | last x == "red" = retrieve (maximum [r, read (head x) :: Int], g, b) xs
-    | last x == "green" = retrieve (r, maximum [g, read (head x) :: Int], b) xs
-    | last x == "blue" = retrieve (r, g, maximum [b, read (head x) :: Int]) xs
-
-power :: (Int, Int, Int) -> Int
-power (r, g, b) = r*g*b
+power :: Int -> Int -> Int -> [[String]] -> Int
+power r g b [] = r*g*b
+power r g b (x:xs)
+    | last x == "red" = power (maximum [r, read (head x) :: Int]) g b xs
+    | last x == "green" = power r (maximum [g, read (head x) :: Int]) b xs
+    | last x == "blue" = power r g (maximum [b, read (head x) :: Int]) xs
 
 solve :: String -> Int
-solve = sum . map power . map (retrieve (0, 0, 0) . map (filter (/= "") . split ' ') . concatMap (split ',') . split ';' . only) . lines
+solve = sum . map (power 0 0 0 . map (filter (/= "") . split ' ') . concatMap (split ',') . split ';' . only) . lines
 
 main :: IO ()
 main = readFile "input.txt" >>= print . solve
