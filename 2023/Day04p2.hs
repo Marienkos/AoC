031d8c9a422a3cc98d6d157c874de189fe423dd1
Marienkos <96983609+Marienkos@users.noreply.github.com>
2023-12-04 22:11:29 +0100

    Update Day04p2.hs

--- 2023/Day04p2.hs
+++ 2023/Day04p2.hs
@@ -34,7 +34,7 @@ autoadd :: [(Int, Int)] -> [(Int, Int)]
 autoadd x = addall ((length x)-1) x
 
 solve :: String -> Int
-solve = sum . map snd .autoadd . map (equal . map (filter (/= "") . split ' ') . split '|'. only) . lines
+solve = sum . map snd . autoadd . map (equal . map (filter (/= "") . split ' ') . split '|'. only) . lines
 
 main :: IO ()
 main = readFile "input.txt" >>= print . solve
