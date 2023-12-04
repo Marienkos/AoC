6f7a751d97e031492e8d35e4df1820b1eda9c486
Marienkos <96983609+Marienkos@users.noreply.github.com>
2023-12-04 22:14:39 +0100

    Add files via upload

---
+++ 2023/Day04.hs
@@ -0,0 +1,28 @@
+isin :: String -> [String] -> Bool
+isin _ [] = False
+isin s (x:xs)
+    | s == x = True
+    | otherwise = isin s xs
+
+split :: Char -> String -> [String]
+split _ [] = []
+split c x = takeWhile (/= c) x : split c (drop 1 (dropWhile (/= c) x))
+
+only :: String -> String
+only [] = []
+only x = case drop 1 (dropWhile (/= ':') x) of
+    "" -> x
+    _ -> drop 1 (dropWhile (/= ':') x)
+
+equal :: [[String]] -> Int
+equal x = length [n | n <- head x, isin n (last x)]
+
+power :: Int -> Int
+power 0 = 0
+power n = 2^(n-1)
+
+solve :: String -> Int
+solve = sum . map (power . equal . map (filter (/= "") . split ' ') . split '|'. only) . lines
+
+main :: IO ()
+main = readFile "input.txt" >>= print . solve
