2dc4999586d4f0ecc1b611d7d97f5e26e0262a58
Marienkos <96983609+Marienkos@users.noreply.github.com>
2023-12-07 15:07:01 +0100

    Update and rename Day06.hs to Day06p2.hs

--- 2023/Day06p2.hs
+++ 2023/Day06p2.hs
@@ -14,5 +14,8 @@ ways time distance = time - (length [x | x <- reverse [0..time], x*(time-x) < di
 innerWays :: [String] -> Int
 innerWays x = ways (read (head x) :: Int) (read (last x) :: Int)
 
+solve :: String -> Int
+solve = innerWays . map (concat . split ' ' . only) . lines
+
 main :: IO ()
-main = readFile "input.txt" >>= print . innerWays . map (concat . split ' ' . only) . lines
+main = readFile "input.txt" >>= print . solve
