2e2274bdfc8a1f1f0ddb84162eda5991dea17553
Marienkos <96983609+Marienkos@users.noreply.github.com>
2023-12-08 16:36:58 +0100

    Add files via upload

---
+++ 2023/Day08one.hs
@@ -0,0 +1,35 @@
+isin :: Char -> [Char] -> Bool
+isin _ [] = False
+isin c (x:xs)
+    | c == x = True
+    | otherwise = isin c xs
+
+split :: String -> [String]
+split [] = []
+split x = takeWhile (/= ' ') x : split (drop 1 (dropWhile (/= ' ') x))
+
+letters :: String -> String
+letters [] = []
+letters (x:xs)
+    | isin x ['A'..'Z'] || x == ' ' = x : letters xs
+    | otherwise = letters xs
+
+search :: String -> [[String]] -> (String, String)
+search _ [] = ("", "")
+search s (x:xs)
+    | head x == s = (x!!1, x!!2)
+    | otherwise = search s xs
+
+direction :: String -> Char -> [[String]] -> String
+direction s 'L' x = fst (search s x)
+direction s 'R' x = snd (search s x)
+
+arrive :: Int -> String -> [Char] -> [[String]] -> Int
+arrive n "ZZZ" _ _ = n
+arrive n s (c:cs) x = arrive (n+1) (direction s c x) cs x
+
+solve :: [String] -> Int
+solve x = arrive 0 "AAA" (concat [head x | n <- [1..]]) $ map (filter (/= "") . split . letters) $ drop 2 x
+
+main :: IO ()
+main = readFile "input.txt" >>= print . solve . lines
