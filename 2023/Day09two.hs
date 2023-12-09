6fc001e0bf41fe4bc8ccd2f5e7033b7e1e8d9c33
Marienkos <96983609+Marienkos@users.noreply.github.com>
2023-12-09 10:35:29 +0100

    Update Day09two.hs

--- 2023/Day09two.hs
+++ 2023/Day09two.hs
@@ -7,15 +7,12 @@ recDiff acc l
     | l == [n | n <- l, n == 0] = (acc, l++[0])
     | otherwise = recDiff (acc++[head l]) (diff l)
 
-gen :: ([Int], [Int]) -> ([Int], [Int])
-gen (acc, l) = (init acc, scanl (-) (last acc) l)
-
-genAll :: ([Int], [Int]) -> Int
-genAll ([], l) = last l
-genAll (acc, l) = genAll (gen (acc, l))
+gen :: ([Int], [Int]) -> Int
+gen ([], l) = last l
+gen (acc, l) = gen (init acc, scanl (-) (last acc) l)
 
 solve :: String -> Int
-solve = sum . map (genAll . recDiff [] . reverse . map (read :: String -> Int) . words) . lines
+solve = sum . map (gen . recDiff [] . reverse . map (read :: String -> Int) . words) . lines
 
 main :: IO ()
 main = readFile "input.txt" >>= print . solve
