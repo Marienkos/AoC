f07ccb550cdf31af0b6acea7eb896a8002efd786
Marienkos <96983609+Marienkos@users.noreply.github.com>
2023-12-14 23:16:53 +0100

    Update Day09one.hs

--- 2023/Day09one.hs
+++ 2023/Day09one.hs
@@ -4,7 +4,7 @@ diff (x1:x2:xs) = (x2-x1) : diff (x2:xs)
 
 recDiff :: [Int] -> [Int] -> ([Int], [Int])
 recDiff acc l
-    | l == [n | n <- l, n == 0] = (acc, l++[0])
+    | l == filter (== 0) l = (acc, l++[0])
     | otherwise = recDiff (acc++[head l]) (diff l)
 
 gen :: ([Int], [Int]) -> Int
