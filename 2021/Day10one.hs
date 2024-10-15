e5635b3c6bf04d3b763c899364dc79eecd4b4dc7
Marienkos <96983609+Marienkos@users.noreply.github.com>
2024-10-15 23:36:48 +0200

    Add files via upload

---
+++ 2021/Day10one.hs
@@ -0,0 +1,35 @@
+simplify :: [Char] -> [Char]
+simplify [a] = [a]
+simplify (a:b:xs) = case [a, b] of
+    "()" -> simplify xs
+    "[]" -> simplify xs
+    "{}" -> simplify xs
+    "<>" -> simplify xs
+    _ -> a : simplify (b:xs)
+
+
+count :: [Char] -> [Char] -> Char
+count open [] = 'A'
+count open (l:ls)
+    | elem l "([{<" = count (l:open) ls
+count (o:os) (l:ls) = case [o, l] of
+    "()" -> count os ls
+    "[]" -> count os ls
+    "{}" -> count os ls
+    "<>" -> count os ls
+    _ -> l
+
+points :: [Char] -> Int
+points [] = 0
+points (x:xs) = case x of
+    'A' -> points xs
+    ')' -> 3 + points xs
+    ']' -> 57 + points xs
+    '}' -> 1197 + points xs
+    '>' -> 25137 + points xs
+
+solve :: [Char] -> Int
+solve = points . map (count []) . lines
+
+main :: IO ()
+main = readFile "input.txt" >>= print . solve
