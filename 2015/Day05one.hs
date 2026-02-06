716cb64c92615ec75410c07278fb6d7901078a75
Marienkos <96983609+Marienkos@users.noreply.github.com>
2026-02-06 01:48:55 +0100

    Add files via upload

---
+++ 2015/Day05one.hs
@@ -0,0 +1,15 @@
+pairs :: String -> Bool
+pairs [_] = False
+pairs (a:b:c) = a == b || pairs (b:c)
+
+banned :: String -> Bool
+banned [_] = True
+banned (a:b:c) = notElem [a, b] ["ab", "cd", "pq", "xy"] && banned (b:c)
+
+nice :: String -> Bool
+nice x = length (filter (flip elem "aeiou") x) > 2 && pairs x && banned x
+
+solve = length . filter nice . lines
+
+main :: IO ()
+main = readFile "input.txt" >>= print . solve
